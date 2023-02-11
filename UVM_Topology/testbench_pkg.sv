a250ee4a60125f5609361a3ae0270abd2a797d55
mddanish05 <125156210+mddanish05@users.noreply.github.com>
2023-02-12 01:46:20 +0530

    Create testbench_pkg.sv

---
+++ UVM_Topology/testbench_pkg.sv
@@ -0,0 +1,10 @@
+package testb_pkg;
+import uvm_pkg::*;
+`include "sequencer.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "agent.sv"
+`include "environment.sv"
+`include "test.sv"
+
+endpackage
